f2d5c9a9b5931283cc52c8c7c44bac145d644f06
wilberquito <wbq.software@gmail.com>
2022-06-01 10:02:07 +0200

    parsing boe using ElementTree

--- boe.py
+++ boe.py
@@ -1,12 +1,29 @@
 import requests
+import xml.etree.ElementTree as ET
 
 date_format = "%Y%m%d"
 target_date = "20141006"
+non_date = '20180101'
 boe_diario_base = "https://www.boe.es/diario_borme/xml.php?id=BORME-S-{date}"
 
 
-if __name__ == '__main__':
-    url = boe_diario_base.format(date=target_date)
-    response = requests.get(url)
-    print(response.content)
+def parse_boe_xml(xml_str):
+    root = ET.fromstring(xml_str)
+    root_tag = root.tag
     
+    if root_tag == 'error':
+        description = root.find('descripcion').text
+        raise Exception('error: ' + description)
+
+
+if __name__ == '__main__':
+    
+    dates = [target_date, non_date]
+    
+    for date in dates:
+        try:
+            url = boe_diario_base.format(date=date)
+            response = requests.get(url)
+            parse_boe_xml(response.text)
+        except Exception as e:
+            print(str(e) + ' Fecha de consulta', date)
